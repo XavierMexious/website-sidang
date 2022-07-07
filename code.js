88ff5a6517deef9e3d8957c857c789b1c41ace85
keefe <nidaleeop27@gmail.com>
2022-07-07 18:18:46 +0700

    login page sisa tombol show password dikasih image

---
+++ code.js
@@ -0,0 +1,8 @@
+function hoverPassword() {
+    var x = document.getElementById("password-input");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+}
